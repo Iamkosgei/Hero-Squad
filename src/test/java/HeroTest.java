b916dcbbb690e061b4661d56778e2be889e5e207
iamkosgei <kosgeik2930@gmail.com>
2019-04-12 10:58:19 +0300

    added test to check whether instance of test are being created correctly

--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -1,5 +1,13 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class HeroTest {
+    @Test
+    public void createsInstanceOfHero()
+    {
+        Hero hero = new Hero("Hulk",30,"Strength","Anger issues");
+        assertEquals(true, hero instanceof Hero);
+    }
 
 }
