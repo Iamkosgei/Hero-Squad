f2bfa3b01634a38e80a9fc69fc645c80547a0df8
iamkosgei <kosgeik2930@gmail.com>
2019-04-12 11:56:07 +0300

    added private fields to squad class

--- src/main/java/Squad.java
+++ src/main/java/Squad.java
@@ -1,2 +1,11 @@
 public class Squad {
+    private int max_size;
+    private String name;
+    private String cause;
+
+    public Squad(int max_size, String name, String cause) {
+        this.max_size = max_size;
+        this.name = name;
+        this.cause = cause;
+    }
 }
