680b53190c326d27d5ec2122461bbe0c284e32f3
iamkosgei <kosgeik2930@gmail.com>
2019-04-12 11:59:46 +0300

    Added test to check if it instantiates correctly

--- src/test/java/SquadTest.java
+++ src/test/java/SquadTest.java
@@ -6,7 +6,8 @@ public class SquadTest {
     @Test
     public void creates_instanceOfSquad()
     {
-
+        Squad squad = new Squad(5,"Squad 1","black lives matter");
+        assertEquals(true, squad instanceof Squad);
     }
 
 }
