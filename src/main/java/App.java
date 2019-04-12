5228806e9b91b8853cddc42eec419540a796f8c9
iamkosgei <kosgeik2930@gmail.com>
2019-04-12 17:26:56 +0300

    Added the categories route

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,30 @@
+import org.apache.log4j.BasicConfigurator;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.Spark.staticFileLocation;
+
 public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+        String layout = "templates/layout.vtl";
+
+        BasicConfigurator.configure();
+
+
+        get("squads", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("squads", Squad.all());
+            model.put("template", "templates/categories.vtl");
+            return new VelocityTemplateEngine().render(
+                    new ModelAndView(model, layout)
+            );
+        });
+
+
+    }
 }
