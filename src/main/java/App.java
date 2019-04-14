4be0d8e27cec661108936cea1c52e47eb73283e5
iamkosgei <kosgeik2930@gmail.com>
2019-04-14 15:30:09 +0300

    deploying to heroku

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -14,6 +14,16 @@ public class App {
 
         BasicConfigurator.configure();
 
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+
+        port(port);
+
 
         get("/", (req, res) -> {
             System.out.println(Squad.all());
